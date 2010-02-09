7a802fc23c9c2463dd7dca92de92b9d8c86bcb01
Paul Sadauskas <psadauskas@gmail.com>
2010-02-08 17:15:11 -0700

    Basic rendering working

--- spec/rest-json-template_spec.rb
+++ spec/rest-json-template_spec.rb
@@ -1,7 +1,46 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "RestJsonTemplate" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+
+  class Article < Struct.new(:id, :title, :text); end
+  
+  class Scope
+    include RestJson::Helper
   end
+
+  def scope
+    scope = Scope.new
+    scope.instance_variable_set(:@article, article)
+    scope
+  end
+
+  def article
+    @article = Article.new(42, "The Answer", "Life, the Universe, and Everything")
+  end
+
+  def data
+    @data ||= File.read(__FILE__).split("\n__END__\n").last
+  end
+
+  def template 
+    @template ||= RestJson::Template.new('test.json.restjson') { data }
+  end
+
+
+  it 'should do something' do
+    text = template.render(scope)
+    pp text
+  end
+
+end
+
+__END__
+restjson_for(@article) do |json|
+
+  json.href "http://example.com/articles/#{@article.id}"
+
+  json.attributes :title, :text
+
+  json.link_to :comments, "http://example.com/articles/#{@article.id}/comments"
+
 end

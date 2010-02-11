72d7f4e773cd7870b812bb0e90b8781f1e25c98d
Paul Sadauskas <psadauskas@gmail.com>
2010-02-10 18:55:17 -0700

    Fix load path

--- lib/rest-json-template.rb
+++ lib/rest-json-template.rb
@@ -1,4 +1,4 @@
 
-require File.expand_path('rest-json-template/builder', __FILE__)
+require File.expand_path('../rest-json-template/builder', __FILE__)
 
 # require rest-json-template/rails or /sinatra
